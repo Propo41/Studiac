51eb6cb81a971b5b6e342b70ba3fdfbd9c08366e
Samia Sabrina Nabi <samia.sabrina.bd2@gmail.com>
2020-03-31 21:23:45 +0600

    phase 1 design completed - samia

---
+++ app/src/main/java/com/example/studiac/SetupprofileChangimg.java
@@ -0,0 +1,14 @@
+package com.example.studiac;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SetupprofileChangimg extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setupprofile_changimg);
+    }
+}
